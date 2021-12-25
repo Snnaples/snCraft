385837dc47dc75deca4b922c88ead869f95f50c8
Snnaples <68153702+Snnaples@users.noreply.github.com>
2021-12-25 19:51:53 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,4 +11,4 @@ client_scripts { "@vrp/client/Tunnel.lua", "@vrp/client/Proxy.lua", 'client.lua'
 
 server_scripts { "@vrp/lib/utils.lua", 'server.lua'	}
 
-files { 'nui/styles.css', 'nui/handler.js', 'nui/index.html', 'nui/img/*.png' } 
+files { 'nui/styles.css', 'nui/handler.js', 'nui/index.html' } 
