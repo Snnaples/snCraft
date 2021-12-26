21a973875b180804d206cdd2cef919a6a5c195f9
Snnaples <68153702+Snnaples@users.noreply.github.com>
2021-12-26 15:41:09 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,14 +1,14 @@
-fx_version "cerulean"
-game  'gta5' 
+fx_version("cerulean")
+game  ('gta5') 
 
-lua54 'TC168'
+lua54 ('TC168')
 
-ui_page 'nui/index.html'
+ui_page ('nui/index.html')
 
-shared_script 'config.lua'
+shared_script ('config.lua')
 
-client_scripts { "@vrp/client/Tunnel.lua", "@vrp/client/Proxy.lua", 'client.lua' }
+client_scripts ({ "@vrp/client/Tunnel.lua", "@vrp/client/Proxy.lua", 'client.lua' })
 
-server_scripts { "@vrp/lib/utils.lua", 'server.lua'	}
+server_scripts ({ "@vrp/lib/utils.lua", 'server.lua'	})
 
-files { 'nui/styles.css', 'nui/handler.js', 'nui/index.html' } 
+files ({ 'nui/styles.css', 'nui/handler.js', 'nui/index.html' })
